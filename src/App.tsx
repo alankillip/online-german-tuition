6ac3a5a46c688c7afb87ca3981ddebd604e06d84
alankillip <alan.kilip@dentsuaegis.com>
2020-06-15 20:19:31 +0100

    format tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import './App.css'
 function App() {
   return (
     <div className="flyer">
-      <div className="section">
+      <div className="section screen-dependent-section-properties">
         <div className="line" />
         <h2 className="item">LEARN GERMAN ONLINE</h2>
         <ul className="item">
@@ -21,13 +21,13 @@ function App() {
           <li>Free trial class</li>
           <li>Competitive hourly rates</li>
         </ul>
-        <img className="item" src={flowers}/>
-        <img className="item flourish" src={flourish}/>
+        <img alt="flowers" className="item" src={flowers}/>
+        <img alt="section end" className="item flourish" src={flourish}/>
       </div>
-      <div className="section item central-text">
+      <div className="section screen-dependent-section-properties">
         <div className="line" />
-        <img className="item germany" src={german}/>
-        <div>
+        <img alt="germany" className="item germany central-text" src={german}/>
+        <div className="central-text vertical-padding">
           After having successfully passed my
           International House certificate in
           September 2002, I have been
@@ -37,16 +37,16 @@ function App() {
           England (London) and in
           Switzerland.
         </div>
-        <h4>GERMAN ONLINE TUITION</h4>
-        <div>
+        <h4 className="central-text">GERMAN ONLINE TUITION</h4>
+        <div className="central-text">
           <p>Email : <a href="mailto:schorrn@gmail.com">schorrn@gmail.com</a></p>
           <p>Mobile : <a href="tel:0044 7570 404 403">0044 7570 404 403</a></p>
           <p><a href="https://www.linkedin.com/in/nadine-schorr-7b7560b9/">LinkedIn</a></p>
         </div>
       </div>
-      <div className="section">
+      <div className="section screen-dependent-section-properties">
         <div className="line" />
-        <div className="item central-text">
+        <div className="item central-text vertical-padding">
           After having successfully passed my
           International House certificate in
           September 2002, I have been
@@ -57,7 +57,7 @@ function App() {
           Switzerland.
         </div>
         <div className="line" />
-        <div className="item central-text">
+        <div className="item central-text vertical-padding">
           Online tuition ensures that you
           learn to speak in a setting that
           reflects your real-world
@@ -74,7 +74,7 @@ function App() {
           course, after just a few weeks you
           will see immediate improvements.
         </div>
-        <img className="item flourish" src={flourish}/>
+        <img alt="section end" className="item flourish" src={flourish}/>
       </div>
     </div>
   );
